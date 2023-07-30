693ff5903f53003bd36a7a2034051697d5ef3d0d
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-07-30 16:26:19 -0300

    utilizando anotações, e criando restApi para tests

--- src/main/java/com/starking/graphql/ForumController.java
+++ src/main/java/com/starking/graphql/ForumController.java
@@ -1,8 +1,38 @@
 package com.starking.graphql;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
 /**
  * @author pedroRhamon
  */
+@Controller
 public class ForumController {
 
+	@Autowired
+	ForumService service;
+
+	@Autowired
+	CommentService commentService;
+
+	//@SchemaMapping(typeName = "Query", value = "postById")
+	@QueryMapping
+	public Post postById(@Argument String id) {
+		return this.service.postById(id);
+	}
+
+	@MutationMapping
+	public Collection<Post> createPost(@Argument String content) {
+		return this.service.createPost(content);
+	}
+
+	@MutationMapping
+	public Collection<Comment> createComment(@Argument String content, @Argument String postId) {
+		return this.commentService.createComment(content, postId);
+	}
 }
